4f8bbedaa81548f13037ebffb09d9b8091893aad
Wildan A <115684223+comzfaza@users.noreply.github.com>
2023-08-02 11:48:33 +0700

    Add files via upload

---
+++ wildanfaza/cobakoneksi.php
@@ -0,0 +1,11 @@
+<?php
+    class rumah{
+        public $warna ="merah";
+        public $pintu = 2;
+        public $jendela = 4;
+
+        public function bukapintu(){
+            echo "pintu telah terbuka";
+        }
+    }
+    $rumah->bukapintu
